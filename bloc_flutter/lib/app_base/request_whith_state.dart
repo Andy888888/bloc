efba5f03d413adeaa5f63a430d7a4c300578f549
燕文强 <yanwenqiang1991@foxmail.com>
2020-07-21 23:51:56 +0800

    send2、send3

--- bloc_flutter/lib/app_base/request_whith_state.dart
+++ bloc_flutter/lib/app_base/request_whith_state.dart
@@ -49,11 +49,119 @@ class RequestWithState<S, T extends StatusModel> {
       },
     );
   }
+
+  void send2(Api<S, T> api,
+      {Function(Api<S, T> api) onStart,
+      Function onCompleted,
+      Function(StateBo<T> data) onSuccess,
+      Function(StateBo<T> data) onFail}) {
+    Request<S, T>(
+      api: api,
+      onStart: (api) {
+        if (onStart != null) onStart(api);
+      },
+      onSuccess: (response) {
+        if (onCompleted != null) onCompleted();
+        onSuccess(StateBo<T>(response.data));
+      },
+      onFail: (response) {
+        if (onCompleted != null) onCompleted();
+        int code = response.data.code;
+        String message = response.data.message;
+        onFail(StateBo.businessFail(code: code, message: message));
+      },
+      onError: (error) {
+        if (onCompleted != null) onCompleted();
+        if (error.runtimeType is DioError) {
+          DioError dioError = error;
+          int statusCode = dioError.response.statusCode + 1000;
+          Net.logFormat('request error status code:$statusCode');
+          onFail(StateBo.networkFail(code: statusCode));
+        } else {
+          Net.logFormat('request error:${error.toString()}');
+          onFail(StateBo.networkFail(code: 119, message: error.toString()));
+        }
+      },
+      onCatchError: (error) {
+        if (onCompleted != null) onCompleted();
+        Net.logFormat('catch error:${error.toString()}');
+        if (error.runtimeType is CastError) {
+          onFail(StateBo.error(code: 110, message: error.toString()));
+        }
+        onFail(StateBo.error());
+      },
+    );
+  }
+
+  static void send3<S, T extends StatusModel>(Api<S, T> api,
+      {Function(Api<S, T> api) onStart,
+      Function onCompleted,
+      Function(StateBo<T> data) onSuccess,
+      Function(StateBo<T> data) onFail}) {
+    Request<S, T>(
+      api: api,
+      onStart: (api) {
+        if (onStart != null) onStart(api);
+      },
+      onSuccess: (response) {
+        if (onCompleted != null) onCompleted();
+        onSuccess(StateBo<T>(response.data));
+      },
+      onFail: (response) {
+        if (onCompleted != null) onCompleted();
+        int code = response.data.code;
+        String message = response.data.message;
+        onFail(StateBo.businessFail(code: code, message: message));
+      },
+      onError: (error) {
+        if (onCompleted != null) onCompleted();
+        if (error.runtimeType is DioError) {
+          DioError dioError = error;
+          int statusCode = dioError.response.statusCode + 1000;
+          Net.logFormat('request error status code:$statusCode');
+          onFail(StateBo.networkFail(code: statusCode));
+        } else {
+          Net.logFormat('request error:${error.toString()}');
+          onFail(StateBo.networkFail(code: 119, message: error.toString()));
+        }
+      },
+      onCatchError: (error) {
+        if (onCompleted != null) onCompleted();
+        Net.logFormat('catch error:${error.toString()}');
+        if (error.runtimeType is CastError) {
+          onFail(StateBo.error(code: 110, message: error.toString()));
+        }
+        onFail(StateBo.error());
+      },
+    );
+  }
 }
 
 test() {
   RequestWithState<String, MyModel>().send<MyApi<String, MyModel>>(
     MyApi.webContent(),
+    onSuccess: (data) {
+      log(data.data.cat);
+    },
+    onFail: (data) {
+      logFormat('code:${data.code}，message:${data.message}');
+    },
+  );
+
+  RequestWithState<String, MyModel>().send2(
+    MyApi.webContent(),
+    onStart: (api) {},
+    onSuccess: (data) {
+      log(data.data.cat);
+    },
+    onFail: (data) {
+      logFormat('code:${data.code}，message:${data.message}');
+    },
+  );
+
+  RequestWithState.send3<String, MyModel>(
+    MyApi.webContent(),
+    onStart: (api) {},
     onSuccess: (data) {
       log(data.data.cat);
     },
