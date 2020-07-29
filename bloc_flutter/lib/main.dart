1de19917dab0d881a4e9a3d347ba086f78820108
燕文强 <yanwenqiang1991@foxmail.com>
2020-07-29 18:14:26 +0800

    logDisable()

--- bloc_flutter/lib/main.dart
+++ bloc_flutter/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:bloc_flutter/page/main/bloc.dart';
 import 'package:bloc_flutter/page/main/page.dart';
 import 'package:flutter/material.dart';
 import 'package:stark/bloc/bloc.dart';
+import 'package:stark/stark.dart';
 import 'page/main/bloc.dart';
 import 'page/main/page.dart';
 
@@ -10,6 +11,7 @@ void main() => runApp(App());
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    logDisable();
     return MaterialApp(
       home: BlocProvider<MainBloc>(bloc: MainBloc(), child: MainPage()),
     );
